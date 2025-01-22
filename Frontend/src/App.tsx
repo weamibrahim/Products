c1b9378d036286b8894a04d7cf7fc89c37e8f13e
weam <weamibrahim46@gmail.com>
2025-01-22 14:31:06 +0200

    remove react from import

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
--- Frontend/src/Pages/viewProducts.jsx
+++ Frontend/src/Pages/viewProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import{ useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaRegEdit } from "react-icons/fa";
 import axios from "axios";
